4d534898218e14e0905c05fd8176aef7031c6c56
Besterbee <sibandabestern@gmail.com>
2025-07-14 13:35:58 +0200

    cors to enable backend on 8080 to communicate with frontend on 5173

--- backend/main.go
+++ backend/main.go
@@ -4,12 +4,22 @@ import (
 	"github.com/gin-gonic/gin"
 	"voting/handlers"
 	"voting/services"
+	"github.com/gin-contrib/cors"
+
 )
 
 func main() {
 	services.Init()
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+	    AllowCredentials: true,
+	}))
+	
 	r.POST("/register", handlers.RegisterVoterHandler)
 	r.POST("/vote", handlers.CastVoteHandler)
 	r.GET("/candidates", handlers.GetCandidatesHandler)
